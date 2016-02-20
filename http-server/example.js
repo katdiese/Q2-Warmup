b794163e65698c6071054149ceb93d906beddb40
Kathy Dieser <katdiese@gmail.com>
2016-02-19 23:43:10 -0700

    practiced creating express generator

--- http-server/example.js
+++ http-server/example.js
@@ -1,12 +1,18 @@
 var http = require('http');
+var port = 8000;
 
 var requestHandler = function(req, res) {
+  var route = req.url;
+  res.end(route);
   console.log("req:", req.url);
   // console.log("res:", res);
+  res.end('route: ' + route);
+  var split = route.split('/');
+  console.log(split[1]);
 }
 
 var server = http.createServer(requestHandler);
 
-server.listen(8080, function() {
-  console.log("...hi");
+server.listen(port, function() {
+  console.log("server is running on port: " + port);
 })
